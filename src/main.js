c5cd9eb766001ba5f6953b0d0b327251f2b48237
LiamPKenna <liampkenna@gmail.com>
2019-11-15 13:35:51 -0800

    import user and log on submit to verify functionality

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import './css/bootstrap.min.css';
 import './css/styles.css';
+import { User } from './js/user.js';
 
 
 // USER INTERFACE
 $(document).ready(function(){
 
+
+
   $('#user-input').submit(event => {
     event.preventDefault();
+    let user = new User(20, "N", 7);
+    console.log(user);
   });
 
 });
