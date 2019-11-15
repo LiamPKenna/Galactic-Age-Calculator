cc2cd013abef5ee01280cede3130f505a5711fac
LiamPKenna <liampkenna@gmail.com>
2019-11-15 14:22:56 -0800

    add planet images and background gif

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import 'bootstrap';
 import './css/bootstrap.min.css';
 import './css/styles.css';
 import { User } from './js/user.js';
+import earth from './img/earth.png';
+import mercury from './img/mercury.png';
+import venus from './img/venus.png';
+import mars from './img/mars.png';
+import jupiter from './img/jupiter.png';
+import sun from './img/sun.png';
 
 
 // USER INTERFACE
@@ -23,5 +29,12 @@ $(document).ready(function(){
     }
 
   });
+
+  $('.earth').append(`<img class"planet" src="${earth}" alt="earth">`);
+  $('.mercury').append(`<img class"planet" src="${mercury}" alt="mercury">`);
+  $('.venus').append(`<img class"planet" src="${venus}" alt="venus">`);
+  $('.mars').append(`<img class"planet" src="${mars}" alt="mars">`);
+  $('.jupiter').append(`<img class"planet" src="${jupiter}" alt="jupiter">`);
+  $('.sun').append(`<img class"sun" src="${sun}" alt="sun">`);
 
 });
